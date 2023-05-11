d8261f16418073f2d0cdc962ae244d20ca1fee82
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-05-12 00:26:42 +0300

    input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,18 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+namespace transport_catalogue {
+namespace detail {
+namespace input {
+
+std::vector<std::pair<std::string_view, int>> ParseDistancesToStops(std::string_view input_info);
+
+std::pair<transport_catalogue::Stop, bool> ParseStopsAndCoord(const std::string& input_info);
+
+transport_catalogue::Bus ParseRouteForBus(std::string_view input_info);
+
+void InputAndOutputInformation(std::istream& input_info);
+
+} //namespace input
+} //namespace detail
+} //namespace transport_catalogue
