1b2e07a0199ad5b6edcf288e05eefd151f675c19
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-05-12 00:33:07 +0300

    stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,16 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+namespace transport_catalogue {
+namespace detail {
+namespace output {
+
+std::string_view ParseBusStatisticsRequest(std::string_view output_info);
+
+std::string_view ParseBusesPassingThroughStopRequest(std::string_view output_info);
+
+void PrintNotFoundBusesAndStops(std::ostream& output, std::string_view stop_name, const std::set<std::string_view>* buses);
+
+}//namespace output
+}//namespace detail
+}//namespace transport_catalogue 
