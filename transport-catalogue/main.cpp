33237c4cad85b6d07cd8554e892311b18698ad5f
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-05-12 00:30:02 +0300

    main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,11 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+ 
+using namespace std;
+using namespace transport_catalogue;
+using namespace transport_catalogue::detail::input; 
+
+int main() {
+   InputAndOutputInformation(std::cin);
+} 
