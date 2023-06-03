54b7cb688f39389e67d8bf8e082565a87d8497fd
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-06-03 10:40:00 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -10,32 +10,35 @@
 #include <numeric>
 #include <optional>
 #include <set>
+#include <functional>
 #include <iomanip>
 #include <string_view>
 #include <unordered_map>
 #include <unordered_set>
 
-
 #include "geo.h"
 
 namespace transport_catalogue {
-
-enum class RouteType { CIRCLE, TWO_DIRECTIONAL };
-
-struct Bus {
-    std::string name_bus;
-    RouteType type;
-    std::vector<std::string_view> stop_names;
-    std::set<std::string_view> unique_stops;
-    size_t GetStopsCount() const;
+    
+enum class RouteType { 
+    CIRCLE, 
+    TWO_DIRECTIONAL
 };
-
+    
 struct Stop {
     std::string name;
     geo::Coordinates coord;
 };
 
 using PairStop = std::pair<const Stop*, const Stop*>;
+using DeqStop = std::deque<const Stop*>;
+using StringVec = std::vector<std::string>;
+    
+struct Bus {
+    RouteType type;
+    std::string name_bus;
+    DeqStop stop_names;
+};
 
 struct DistanceHasher {
     std::size_t operator()(const PairStop pair_stops) const noexcept {
@@ -46,27 +49,36 @@ private:
 };
 
 struct BusQueryInput {
-    std::string_view number;
+    bool bus_not_found_ = false;
+    std::string number;
     size_t stops_count = 0;
     size_t unique_stops_count = 0;
-    int rout_length = 0;
+    double route_length = 0.0;
     double curvature = 0.0;
 };
 
-std::ostream& operator<<(std::ostream& output, const BusQueryInput& bus_info);
-
+struct StopQueryInput {
+    bool not_found_ = false;
+    std::string name;
+    std::set<std::string_view> buses_name;
+};
+    
 class TransportCatalogue {
 
 public:  
-    void AddStop(Stop stop);
-    void AddBus(Bus bus);
-    void AddDistance(std::string_view stop_from, std::string_view stop_to, int distance);
-    std::optional<BusQueryInput> GetBusInfo(std::string_view bus) const;
-    const std::set<std::string_view>* GetBusesOnTheStop(std::string_view stop_name) const;
+    void AddStop(const Stop& stop);
+    void AddBus(const Bus& bus);
+    void SetDistance(std::string_view stop_from, std::string_view stop_to, size_t distance);
+    BusQueryInput GetBusInfo(std::string_view bus) const;
+    StopQueryInput GetStopInfo(std::string_view stop_name) const;
+    const Bus* FindBus(const std::string_view bus_name) const;
+    const Stop* FindStop(const std::string_view stop_name) const;
+    size_t GetDistance(std::string_view stop_from, std::string_view stop_to) const;
     
 private:
-    int CalculateRouteLength(const Bus* bus_info) const;
-    double CalculateGeographicLength(const Bus* bus_info) const;
+    double CalculateRouteLength(std::string_view bus_info) const;
+    double CalculateGeographicLength(std::string_view bus_info) const;
+    size_t CalculateUniqueStops(std::string_view bus) const;
 
 private: 
     std::deque<Stop> stops;
@@ -74,7 +86,7 @@ private:
     std::unordered_map<std::string_view, const Stop*> map_all_stops;
     std::unordered_map<std::string_view, const Bus*> map_all_buses;
     std::unordered_map<PairStop, int, DistanceHasher> map_distance_to_stop;
-    std::unordered_map<std::string_view, std::set<std::string_view>> stop_to_bus_map;
+    std::unordered_map<const Stop*, std::set<std::string_view>> stop_to_bus_map; // проще было так реализовать, по - моему... 
     
 }; //TransportCatalogue
 }  //transport_catalogue
