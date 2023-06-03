17de84a779cd73bb058ba39d29296faf05c74887
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-06-03 10:43:25 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -4,13 +4,15 @@
 namespace transport_catalogue {
 namespace detail {
 namespace output {
+    
+void PrintBusInfo(std::ostream& output, const BusQueryInput& bus_info);
 
-std::string_view ParseBusStatisticsRequest(std::string_view output_info);
+void PrintStopInfo(std::ostream& output, const StopQueryInput& stop_info);
 
-std::string_view ParseBusesPassingThroughStopRequest(std::string_view output_info);
+std::string_view ParseRequest(std::string_view output_info);
 
-void PrintNotFoundBusesAndStops(std::ostream& output, std::string_view stop_name, const std::set<std::string_view>* buses);
+void PrintFinalResult(const TransportCatalogue& catalogue, std::istream& input);
 
 }//namespace output
 }//namespace detail
-}//namespace transport_catalogue 
+}//namespace transport_catalogue
