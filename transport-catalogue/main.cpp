c05ef3cc584863a912451ddc5e42a2a3fad553ca
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-06-03 10:47:50 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,11 +1,16 @@
 #include "transport_catalogue.h"
 #include "input_reader.h"
 #include "stat_reader.h"
- 
+
+
+
 using namespace std;
 using namespace transport_catalogue;
 using namespace transport_catalogue::detail::input; 
+using namespace transport_catalogue::detail::output;
 
 int main() {
-   InputAndOutputInformation(std::cin);
+   TransportCatalogue catalogue;
+   InputInformation(catalogue, std::cin);
+   PrintFinalResult(catalogue, std::cin);
 } 
