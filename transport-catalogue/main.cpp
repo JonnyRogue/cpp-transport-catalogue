198fa152915580b051a6e70965bca5e17a2a46fd
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-06-14 23:28:40 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,16 +1,18 @@
-#include "transport_catalogue.h"
-#include "input_reader.h"
-#include "stat_reader.h"
-
-
+#include "json_reader.h"
 
 using namespace std;
 using namespace transport_catalogue;
-using namespace transport_catalogue::detail::input; 
-using namespace transport_catalogue::detail::output;
 
 int main() {
-   TransportCatalogue catalogue;
-   InputInformation(catalogue, std::cin);
-   PrintFinalResult(catalogue, std::cin);
-} 
+
+	TransportCatalogue catalogue;
+	renderer::MapRenderer map_renderer;
+	request::RequestHandler request_handler(catalogue);
+	json::JSONReader json_reader(catalogue, request_handler, map_renderer);
+
+	json_reader.ReadJSON(std::cin);
+	json_reader.BuildDataBase();
+	json_reader.GenerateAnswer();
+	json_reader.PrintAnswer(std::cout);
+	
+}
