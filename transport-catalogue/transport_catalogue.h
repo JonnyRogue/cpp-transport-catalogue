3e51d169e36ddb46933fb3980f72eb31f650a19f
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-06-14 23:29:46 +0300

    Update transport_catalogue.h
    11

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,44 +1,7 @@
 #pragma once
-#include <iostream>
-#include <string>
-#include <vector>
-#include <cassert>
-#include <regex>
-#include <deque>
-#include <list>
-#include <execution>
-#include <numeric>
-#include <optional>
-#include <set>
-#include <functional>
-#include <iomanip>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-
-#include "geo.h"
+#include "domain.h"
 
 namespace transport_catalogue {
-    
-enum class RouteType { 
-    CIRCLE, 
-    TWO_DIRECTIONAL
-};
-    
-struct Stop {
-    std::string name;
-    geo::Coordinates coord;
-};
-
-using PairStop = std::pair<const Stop*, const Stop*>;
-using DeqStop = std::deque<const Stop*>;
-using StringVec = std::vector<std::string>;
-    
-struct Bus {
-    RouteType type;
-    std::string name_bus;
-    DeqStop stop_names;
-};
 
 struct DistanceHasher {
     std::size_t operator()(const PairStop pair_stops) const noexcept {
@@ -46,21 +9,6 @@ struct DistanceHasher {
     }
 private:
     std::hash<const void*> hasher_;
-};
-
-struct BusQueryInput {
-    bool bus_not_found_ = false;
-    std::string number;
-    size_t stops_count = 0;
-    size_t unique_stops_count = 0;
-    double route_length = 0.0;
-    double curvature = 0.0;
-};
-
-struct StopQueryInput {
-    bool not_found_ = false;
-    std::string name;
-    std::set<std::string_view> buses_name;
 };
     
 class TransportCatalogue {
@@ -86,7 +34,7 @@ private:
     std::unordered_map<std::string_view, const Stop*> map_all_stops;
     std::unordered_map<std::string_view, const Bus*> map_all_buses;
     std::unordered_map<PairStop, int, DistanceHasher> map_distance_to_stop;
-    std::unordered_map<const Stop*, std::set<std::string_view>> stop_to_bus_map; // проще было так реализовать, по - моему... 
+    std::unordered_map<const Stop*, std::unordered_set<const Bus*>> stop_to_bus_map; 
     
 }; //TransportCatalogue
 }  //transport_catalogue
