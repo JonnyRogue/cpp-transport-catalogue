d814c2cd68fb9d592716a1cfba1c113ec1da70b4
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-07-23 18:20:28 +0300

    Update request_handler.cpp
    13 sprint

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -1,9 +1,8 @@
 #include "request_handler.h"
 
 namespace transport_catalogue {
-namespace request {
-    
-RequestHandler::RequestHandler(const TransportCatalogue& db) : db_(db) {}
+
+RequestHandler::RequestHandler(const TransportCatalogue& db, const renderer::MapRenderer& renderer, const TransportRouter& router) : db_(db), renderer_(renderer), router_(router) {}
 
 std::optional<BusQueryInput> RequestHandler::GetBusStat(const std::string_view& bus_name) const {
     return db_.GetBusInfo(bus_name);
@@ -13,6 +12,27 @@ const std::unordered_set<const Bus*> RequestHandler::GetBusesByStop(const std::s
     const std::unordered_set<const Bus*> a = db_.GetStopInfo(stop_name).buses_name;
     return a;
 }
+
+const std::deque<const Stop*>& RequestHandler::GetStopsByBus(const std::string_view bus_name) const {
+	return db_.FindBus(bus_name)->stop_names;	
+}
+
+const std::unordered_set<const Bus*> RequestHandler::GetAllBuses() const {
+	return db_.GetAllBuses();
+}
+
+const std::unordered_set<const Stop*> RequestHandler::GetAllStops() const {
+	return db_.GetAllStops();
+}
+
+svg::Document RequestHandler::RenderMap(const renderer::RenderSettings& render_settings,
+	const std::set< const Bus*, CompareBus >& buses) const {
+	return renderer_.RenderMap(render_settings, buses);
+}
+
+std::optional<graph::Router<double>::RouteInfo> RequestHandler::BuildRoute(
+	std::string_view stop_from, std::string_view stop_to) const {
+	return router_.BuildRoute(stop_from, stop_to);
+}
     
-} //namespace request
 } //namespace transport_catalogue
