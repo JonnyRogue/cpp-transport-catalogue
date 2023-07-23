99419eedbd0a1dc017c1ee1689de790f9618a217
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-07-23 17:52:34 +0300

    Update json.h
    13 sprint

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -10,48 +10,13 @@ class Node;
 using Dict = std::map<std::string, Node>;
 using Array = std::vector<Node>;
 
-struct PrintContext {
-    std::ostream& out;
-    int indent_step = 4;
-    int indent = 0;
-    
-    PrintContext(std::ostream& out);
-    
-    PrintContext(std::ostream& out, int indent_step, int indent = 0);
-    
-    void PrintIndent() const {
-        for (int i = 0; i < indent; ++i) {
-            out.put(' ');
-        }
-    }
-    
-    PrintContext Indented() const {
-        return { out, indent_step, indent_step + indent };
-    }
-};
-
-template <typename Value>
-void PrintValue(const Value& value, const PrintContext& text) {
-    text.out << value;
-}
-
-void PrintValue(std::nullptr_t, const PrintContext& text);
-
-void PrintValue(bool value, const PrintContext& text);
-
-void PrintValue(const Array& value, const PrintContext& text);
-
-void PrintValue(const Dict& value, const PrintContext& text);
-
-void PrintNode(const Node& node, std::ostream& out);
-
 class ParsingError : public std::runtime_error {
 public:
     using runtime_error::runtime_error;
 };
 
 class Node final
-        : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> { // достаточно ли этого? или вы имели ввиду что-то другое?
+        : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> { 
 public:
     using variant::variant;
     using Value = variant;
@@ -73,28 +38,34 @@ public:
     bool IsMap() const;
     const Value& GetValue() const;
     bool operator==(const Node& other) const;
-    bool operator!=(const Node& other) const;
+    Value& GetNoConstValue();
 
 };
 
-std::ostream& operator<<(std::ostream& out, const Node& node);
+inline bool operator!=(const Node& lhs, const Node& rhs) {
+    return !(lhs == rhs);
+}
 
 class Document {
 public:
     explicit Document();
     explicit Document(Node root);
     const Node& GetRoot() const;
-    bool operator==(const Document& other) const;
-    bool operator!=(const Document& other) const;
-
 private:
     Node root_;
 };
 
-Document Load(std::istream& input);
+inline bool operator==(const Document& lhs, const Document& rhs) {
+    return lhs.GetRoot() == rhs.GetRoot();
+}
 
-void Print(const Document& doc, std::ostream& output);
+inline bool operator!=(const Document& lhs, const Document& rhs) {
+    return !(lhs == rhs);
+}
+
+Document Load(std::istream& input);
     
+void Print(const Document& doc, std::ostream& output);
  
 }// end namespace json
 }// end namespace transport_catalogue
