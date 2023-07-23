7eb73833992ac9ef6f2efb6839a7996e0a9b0ffe
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-07-23 18:13:38 +0300

    Update main.cpp
    13 sprint

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,18 +1,23 @@
+#include "json.h"
 #include "json_reader.h"
-
-using namespace std;
-using namespace transport_catalogue;
+#include "request_handler.h"
+#include "transport_router.h"
 
 int main() {
-
-	TransportCatalogue catalogue;
-	renderer::MapRenderer map_renderer;
-	request::RequestHandler request_handler(catalogue);
-	json::JSONReader json_reader(catalogue, request_handler, map_renderer);
-
-	json_reader.ReadJSON(std::cin);
-	json_reader.BuildDataBase();
-	json_reader.GenerateAnswer();
-	json_reader.PrintAnswer(std::cout);
+    using namespace std;
+    using namespace transport_catalogue;
+    using namespace transport_catalogue::json;
+    
+    TransportCatalogue catalogue;
+    renderer::MapRenderer map_renderer;
+    JSONReader json_reader(catalogue, map_renderer);
+    Data data = std::move(json_reader.ReadJSON(std::cin));
+    TransportRouter transport_router(catalogue, data.routing_settings);
+    RequestHandler request_handler(catalogue, map_renderer, transport_router);
+    json_reader.BuildDataBase(data);
+    transport_router.BuildGraphAndRouter();
+    Document answers = std::move(json_reader.GenerateAnswer(transport_router, data.stat_requests));
+    Print(answers, std::cout);
 	
+    return 0;
 }
