2bbab45988da2f19c1506cc39055a408f35a3c00
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-08-27 09:16:33 +0300

    Update map_renderer.h
    sprint 15

--- transport-catalogue/map_renderer.h
+++ transport-catalogue/map_renderer.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "geo.h"
 #include "svg.h"
 #include "domain.h"
@@ -6,30 +7,58 @@
 namespace transport_catalogue {
 namespace renderer {
 
-using BusesCoordinates = std::map<const Bus*, std::vector<geo::Coordinates>, CompareBus>;
 inline const double EPSILON = 1e-6;
 bool IsZero(double value);
     
 struct RenderSettings {
-    double width = 200;
-    double height = 200;
-    double padding = 30;
-    double line_width = 14;
-    double stop_radius = 5;
-    int bus_label_font_size = 20;
-    std::vector<double> bus_label_offset{7, 15};
-    int stop_label_font_size = 20;
-    std::vector<double> stop_label_offset{7, -3};
-    svg::Color underlayer_color = svg::Rgba{ 255, 255, 255, 0.85 };
-    double underlayer_width = 3;
-    std::vector<svg::Color> color_palette{ "green", svg::Rgb{ 255,160,0 }, "red" };
+    double width;
+    double height;
+    double padding;
+    double line_width;
+    double stop_radius;
+    uint32_t bus_label_font_size;
+    transport_catalogue::svg::Point bus_label_offset;
+    uint32_t stop_label_font_size;
+    transport_catalogue::svg::Point stop_label_offset;
+    svg::Color underlayer_color;
+    double underlayer_width;
+    std::vector<svg::Color> color_palette;
 };
 
 class SphereProjector {
 public:
     template <typename PointInputIt>
-    SphereProjector(PointInputIt points_begin, PointInputIt points_end, double max_width, double max_height, double padding);
+    SphereProjector(PointInputIt points_begin, PointInputIt points_end, double max_width, double max_height, double padding) : padding_(padding) {
+        if (points_begin == points_end) {
+            return;
+        }
+        const auto [left_it, right_it] = std::minmax_element(points_begin, points_end, [](auto lhs, auto rhs) { 
+            return lhs.lng < rhs.lng; });
+            min_lon_ = left_it->lng;
+            const double max_lon = right_it->lng;
+            const auto [bottom_it, top_it] = std::minmax_element(points_begin, points_end, [](auto lhs, auto rhs) { 
+                return lhs.lat < rhs.lat; });
+                const double min_lat = bottom_it->lat;
+                max_lat_ = top_it->lat;
+                std::optional<double> width_zoom;
+                if (!IsZero(max_lon - min_lon_)) {
+                    width_zoom = (max_width - 2 * padding) / (max_lon - min_lon_);
+                }
+                std::optional<double> height_zoom;
+                if (!IsZero(max_lat_ - min_lat)) {
+                    height_zoom = (max_height - 2 * padding) / (max_lat_ - min_lat);
+                }
+                if (width_zoom && height_zoom) {
+                    zoom_coeff_ = std::min(*width_zoom, *height_zoom);
+                } else if (width_zoom) {
+                      zoom_coeff_ = *width_zoom;
+                  } else if (height_zoom) {
+                        zoom_coeff_ = *height_zoom;
+                    }
+    }
+    
     svg::Point operator()(geo::Coordinates coords) const;
+    
 
 private:
     double padding_;
@@ -40,46 +69,21 @@ private:
     
 class MapRenderer {
 public:
-    svg::Document RenderMap(const renderer::RenderSettings& render_settings, const std::set< const Bus*, CompareBus >& buses) const;
+    explicit MapRenderer(const renderer::RenderSettings& render_settings) : settings_(render_settings) {}
+    void RenderSvgMap(const transport_catalogue::TransportCatalogue& catalog, std::ostream& out);
 
 private:
-    std::pair<BusesCoordinates, std::vector<geo::Coordinates>> HighlightBusesAndCoordinatesOfStops(const std::set< const Bus*, CompareBus >& buses) const;
-    svg::Document CreateVisual(const renderer::RenderSettings& render_settings, const std::set< const Bus*, CompareBus >& buses, const BusesCoordinates& bus_geo_coords, const SphereProjector projector) const;
-    svg::Document CreatePolylinesRoutes(const renderer::RenderSettings& render_settings, const BusesCoordinates& bus_geo_coords, const SphereProjector projector) const;
-    void SetPathPropsAttributesPolyline(const renderer::RenderSettings& render_settings, svg::Polyline& polyline, size_t count) const;
-    void CreateRouteNames(const renderer::RenderSettings& render_settings, const std::set< const Bus*, CompareBus >& buses, svg::Document& document, const SphereProjector projector) const;
-    std::pair<svg::Text, svg::Text> CreateBackgroundAndTitleForRoute(const renderer::RenderSettings& render_settings, svg::Point point, std::string_view bus_name, size_t count) const;
-    void CreateStopIcons(const renderer::RenderSettings& render_settings, svg::Document& document, const std::set<const Stop*, CompareStop>& stops, const SphereProjector projector) const;
-    void CreateStopNames(const renderer::RenderSettings& render_settings, svg::Document& document, const std::set<const Stop*, CompareStop>& stops, const SphereProjector projector) const;
+    const RenderSettings settings_;
+    SphereProjector* projector_ = nullptr;
+    const std::map<std::string_view, const transport_catalogue::Bus*>* buses_ = nullptr;
+    const std::map<std::string_view, const transport_catalogue::Stop*>* stops_ = nullptr;
+    svg::Color GetNextPalleteColor(size_t &color_count) const;
+    svg::Color GetPalletColor(size_t route_number) const;
+    void RenderLines(svg::Document& svg_doc) const;
+    void RenderRouteNames(svg::Document& svg_doc) const;
+    void RenderStopCircles(const transport_catalogue::TransportCatalogue& catalog, svg::Document& svg_doc) const;
+    void RenderStopNames(const transport_catalogue::TransportCatalogue& catalog, svg::Document& svg_doc) const;
 };
-
-template <typename PointInputIt>
-SphereProjector::SphereProjector(PointInputIt points_begin, PointInputIt points_end, double max_width, double max_height, double padding) : padding_(padding) {
-    if (points_begin == points_end) {
-        return;
-    }
-    const auto [left_it, right_it] = std::minmax_element(points_begin, points_end, [](auto lhs, auto rhs) { return lhs.lng < rhs.lng; });
-    min_lon_ = left_it->lng;
-    const double max_lon = right_it->lng;
-    const auto [bottom_it, top_it] = std::minmax_element(points_begin, points_end, [](auto lhs, auto rhs) { return lhs.lat < rhs.lat; });
-    const double min_lat = bottom_it->lat;
-    max_lat_ = top_it->lat;
-    std::optional<double> width_zoom;
-    if (!IsZero(max_lon - min_lon_)) {
-        width_zoom = (max_width - 2 * padding) / (max_lon - min_lon_);
-    }
-    std::optional<double> height_zoom;
-    if (!IsZero(max_lat_ - min_lat)) {
-        height_zoom = (max_height - 2 * padding) / (max_lat_ - min_lat);
-    }
-    if (width_zoom && height_zoom) {
-        zoom_coeff_ = std::min(*width_zoom, *height_zoom);
-    } else if (width_zoom) {
-          zoom_coeff_ = *width_zoom;
-      } else if (height_zoom) {
-            zoom_coeff_ = *height_zoom;
-        }
-}
 
 } //namespace renderer  
 } //namespace transport_catalogue
